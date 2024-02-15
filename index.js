7366caf4ea6a554fca1f5b89497a4624314d4953
tenor2000 <tenor2000@gmail.com>
2024-02-15 13:44:12 -0500

    Updated portfolio

--- index.js
+++ index.js
@@ -121,3 +121,9 @@ function clearDescription() {
 }
 
 getData();
+
+window.addEventListener('scroll', function() {
+    let scrolled = window.scrollY;
+    let parallax = document.querySelector('.parallax');
+    parallax.style.transform = 'translateY(' + (scrolled * 0.5) + 'px)';
+});
